ec90288d924cc66e6427d96db6a512f69904e5c2
Sheldon Clark <sheldonclark@outlook.com>
2016-10-18 13:42:32 -0400

    print answer to exercise 2 questions

---
+++ exercise2.rb
@@ -0,0 +1,10 @@
+#1.How would you calculate a good tip for a 55 dollar meal?
+meal_price=55*1.25
+puts meal_price
+#2.Find a way to convert an integer into a string and add another string
+puts "I am " + 200.to_s
+#3.Output the resule of 45628 multiplied by 7839 in a sentence
+sum=45628*7839
+puts "the sum of 45628 multiplied by 7839 is #{sum}"
+#what's the value of (10<20&&30<20)||!(10==11)?
+puts (10<20&&30<20)||!(10==11)
