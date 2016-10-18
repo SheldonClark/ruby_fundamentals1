0e987625d0505829b95cacb7d5985dd5914c6ac0
Sheldon Clark <sheldonclark@outlook.com>
2016-10-18 14:29:49 -0400

    output the users birth year instead of age

--- exercise3.rb
+++ exercise3.rb
@@ -2,4 +2,5 @@ puts "what is your name?"
   users_name=gets
 puts "How old are you?"
   users_age=gets.to_i
-puts "Your name is #{users_name} and you are #{users_age} years old."
+  birth_year=2016 - users_age
+puts "Your name is #{users_name} and you were born in#{birth_year}."
